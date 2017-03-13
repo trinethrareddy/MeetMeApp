de0cdea6f1ff64d6deab08e24b938ae449597e00
INNOMINDSHYD\risetty <risetty@innominds.com>
2017-03-13 16:26:04 +0530

    facebook login

--- app/modules/login/loginController.js
+++ app/modules/login/loginController.js
@@ -1,9 +1,23 @@
-(function(){
-	'use strict';
-	angular
-	.module('meetMeApp.login')
-	.controller('loginCtrl',['$scope',function($scope){
-		$scope.name ="namratha";
-		console.log("123");
-	}]);
+(function() {
+    'use strict';
+    angular
+        .module('meetMeApp.login')
+        .controller('loginCtrl', ['$scope', function($scope) {
+            $scope.name = "namratha";
+            console.log("123");
+            $scope.fbLogin = function() {
+            	console.log("fbLogin");
+                FB.login(function(response) {
+                    if (response.authResponse) {
+                        console.log('Welcome!  Fetching your information.... ');
+                        FB.api('/me', function(response) {
+                            console.log('Good to see you, ' + response.name + '.');
+                        });
+                    } else {
+                        console.log('User cancelled login or did not fully authorize.');
+                    }
+                });
+            }
+
+        }]);
 })();
