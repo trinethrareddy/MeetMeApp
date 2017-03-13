e3e2a9e9da80b8ae2d8e7caef1ff5bb0784b6b4b
namratha <namratha.k7@gmail.com>
2017-03-13 23:54:26 +0530

    google n fb login

--- app/modules/login/loginController.js
+++ app/modules/login/loginController.js
@@ -12,6 +12,7 @@
 		};
 
 		$scope.onGoogleLogin = function(){
+			console.log("abc");
 			var params = {
 				'clientid' : '818379217611-rfohfqt4k2m20pvqvdanselgeo209th0.apps.googleusercontent.com',
 				'cookiepolicy' : 'single_host_origin',
@@ -27,6 +28,7 @@
 							$scope.$apply(function(){
 								$scope.gmail.username = resp.displayName;
 								$scope.gmail.email = resp.emails[0].value;
+								$scope.g_image = resp.image.url;
 							}); 
 						});
 					}
@@ -37,5 +39,31 @@
 
 			gapi.auth.signIn(params);
 		}
+
+		$scope.facebook = {
+			username : "",
+			email : ""
+		};
+
+		$scope.onFBLogin = function(){
+			FB.login(function(response){
+				if(response.authResponse){
+					FB.api('/me','GET',{fields:'email, first_name , name , id, picture'},function(response){
+						$scope.$apply(function(){
+								$scope.facebook.username = response.name;
+								$scope.facebook.email = response.email;
+								$scope.fb_image = response.picture.data.url;
+							}); 
+
+					});
+				} else {
+					//error
+				}
+			},{
+				scope: 'email , user_likes',
+				return_scopes : true
+			});
+		}
+
 	}]);
 })();
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -65,3 +65,32 @@ function onLoadFunction(){
     gapi.client.setApiKey('AIzaSyDeXfVywR8aZjQOKNl7mi05CJfdtR5a8E8');
     gapi.client.load('plus','v1',function(){});
 }
+
+// //Facebook Login
+//  window.fbAsyncInit = function() {
+//     FB.init({
+//       appId      : '2001063750121137',
+//       xfbml      : true,
+//       version    : 'v2.8',
+//       status : true
+//     });
+
+//     FB.getLoginStatus(function(response){
+//         if(response.status === 'connected')
+//             {
+//                 //we are coonected
+//             } else if(response.status === 'not_authorized')  {
+//                 //not auth
+//             } else {
+//                 //we are not logged
+//             }
+//              });
+//             };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
